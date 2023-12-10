6551d05c86fc7f20e5922f19742fcd642a809328
souichi-ichikawa <21j5011@stu.meisei-u.ac.jp>
2023-12-10 12:03:11 +0900

    ブログ投稿詳細の表示

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController; 
-
+use App\Http\Controllers\PostController;      //追加
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +12,5 @@ use App\Http\Controllers\PostController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {return view('welcome');});
-Route::get('/posts', [PostController::class, 'index']); 
+Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/{post}', [PostController::class ,'show']);
